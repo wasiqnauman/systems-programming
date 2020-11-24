06697250b5c09bfd650bcac4151e0da45f1b54c7
wasiq <wasiq_nauman@hotmail.com>
2020-11-24 22:54:28 +0500

    update

--- myls.c
+++ myls.c
@@ -3,7 +3,17 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <string.h>
-int main(int argc, char* argv[]) {
+#include <sys/stat.h>
+
+
+char* get_type(char d_type) {
+    if(d_type == DT_DIR)
+        return "D";
+    if(d_type == DT_REG)
+        return "F";
+}
+void run_ls(int argc, char* argv[]) {
+    struct stat buf;
     char* name = malloc(sizeof(char[100]));
     if(argc >= 2) {
         strcpy(name, argv[1]);
@@ -17,9 +27,49 @@ int main(int argc, char* argv[]) {
     }
     struct dirent* entry;
     while((entry = readdir(dir)) != NULL){
-        printf("%s\n", entry->d_name);
+        stat(entry->d_name, &buf);
+        char type[100];
+        //char* type = get_type(entry->d_type);
+        if(S_ISDIR(buf.st_mode))
+            strcpy(type, "D");
+        if(S_ISREG(buf.st_mode))
+            strcpy(type, "F");
+        printf("%s: %s\n",type, entry->d_name);
     }
     perror("readdir");
     closedir(dir);
-    return 0;
+
 }
+void scanFunc(int argc, char* argv[]) {
+struct dirent** names;
+    int n;
+    if(argc < 2)
+        argv[1] = ".";
+    n = scandir(argv[1], &names, NULL, alphasort);
+    if(n < 0)
+        perror("scandir\n");
+    else
+    {
+        for(int i=0; i<n; i++) {
+            struct stat* buf;
+            int r = stat(names[i]->d_name, buf);
+            if(r< 0) {
+                perror("stat\n");
+                exit(1);
+            }
+            char type[100];
+            if(S_ISREG(buf->st_mode))
+                strcpy(type, "F");
+            if(S_ISDIR(buf->st_mode))
+                strcpy(type, "D");
+            //char* type = get_type(names[i]->d_type);
+            printf("%s: %s\n", type, names[i]->d_name);
+            free(names[i]);
+        }
+    }
+
+}
+int main(int argc, char* argv[]){
+    run_ls(argc, argv);
+}
+
